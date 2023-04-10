24b08531bb374dbdb630fceca02a53e201781163
App Generator <51070104+app-generator@users.noreply.github.com>
2023-04-10 10:35:08 +0300

    Changes for User profile

--- apps/home/views.py
+++ apps/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -8,3 +8,11 @@ from django.http import HttpResponse
 def index(request):
     # Page from the theme
     return render(request, "pages/index.html")
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+  context = {
+    'segment': 'profile',
+  }
+  return render(request, 'pages/profile.html', context)
